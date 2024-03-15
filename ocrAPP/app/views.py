038e4da470d79bfc291bf109ebdc612da2a9c0c5
Anish Khatiwada <np03cs4s220208@heraldcollege.edu.np>
2024-03-15 22:38:56 +0545

    Adding footer

--- ocrAPP/app/views.py
+++ ocrAPP/app/views.py
@@ -10,8 +10,8 @@ def home(request):
         image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), cv2.IMREAD_UNCHANGED)
 
         # Model path and vocabulary list
-        model_path = "Model/202402171624/model.onnx"
-        vocab = ['i', '3', 'O', 'C', 'P', '1', 'p', ',', 'H', '?', '6', 'x', '-', '8', 'g', 'T', 't', '!', 'c', 'G', 'U', 'D', 'V', 'R', 'a', 'u', '#', 'Y', 'Q', '.', ':', '9', 'K', 'b', 'I', 'r', '(', 'j', '&', 'f', 'n', 'Z', 'd', '5', 'L', 'J', '4', 'B', 'v', 'w', ')', "'", 'N', 'm', 'M', 'h', 'k', '*', '7', 'F', 'S', '+', '2', 'q', '"', '0', 'X', 'z', ' ', 'o', 'W', 'e', ';', 'y', 'E', 'A', 's', '/', 'l']
+        model_path = "Model/202403031722/model.onnx"
+        vocab = [')', 'D', 'E', 't', '(', 'U', '8', 'r', 'X', 'P', 'i', 'e', '"', '/', '-', ' ', '5', ':', '&', 'R', '.', 'u', '0', 'h', 'Q', 'H', 'N', 'L', 'S', '#', 'd', 'z', 'M', 'W', 'b', 'K', 'T', 'v', 'Y', 'j', 'w', 'n', 'F', 'J', '4', '!', '*', 'y', 'o', ',', '?', 'C', '7', '3', 'g', 'V', 'm', 's', 'c', 'a', '6', 'G', 'q', 'f', 'p', 'O', "'", '2', 'Z', 'k', 'B', '1', '+', 'l', 'x', ';', '9', 'A', 'I']
 
         # Create the model
         model = ImageToWordModel(model_path=model_path, vocab=vocab)
